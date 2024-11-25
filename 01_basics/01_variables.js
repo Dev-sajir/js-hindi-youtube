1fa26ac113e2007fffe59991434bd6e6c26e179e
Code with Emam <144737865+Dev-sajir@users.noreply.github.com>
2024-11-25 11:05:44 +0000

    js const let and console done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,17 @@
-const 
+// Fixed value should be in const
+// Changeable values should be in let
+// var is no longer used. bcz of block scope and block function issues
+// console.table to display multiple info in table format
+// Values can be reassign in let. 
+
+const accountID = 121;
+let accountEmail = "Sajir@gmail.com";
+let accountPassword = "12345";
+let accountCity = "Bangladesh";
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+accountEmail = "hmSajir@gmail.com";
+accountPassword = "61234115";
+accountCity = "india";
+console.table([accountID, accountEmail, accountPassword, accountCity]);
